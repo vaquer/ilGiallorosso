2100a5fde94a311ab6c3281d69acf369e1315221
vaker <akura11.tt@gmail.com>
2016-01-27 19:46:26 +0000

    allowing all tags

--- ilGiallorosso/static/js/admin/textareaentry.js
+++ ilGiallorosso/static/js/admin/textareaentry.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     if($("#id_text").length > 0){
         CKEDITOR.replace('id_text', {
             language: 'es',
+            allowedContent: true,
             uiColor: '#9AB8F3'
         });
     }
@@ -9,6 +10,7 @@ $(document).ready(function(){
     if($("#id_short_about").length > 0){
         CKEDITOR.replace('id_short_about', {
             language: 'es',
+            allowedContent: true,
             uiColor: '#9AB8F3'
         });
     }
