67b35520abd38b9b2a658c7ec2145d720aee0a77
vaker <akura11.tt@gmail.com>
2016-01-27 01:10:05 +0000

    improve short bio textbox in author admin view

--- ilGiallorosso/static/js/admin/textareaentry.js
+++ ilGiallorosso/static/js/admin/textareaentry.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-	CKEDITOR.replace('id_text', {
-		language: 'es',
-		uiColor: '#9AB8F3'
-	});	
+    CKEDITOR.replace('id_text', {
+        language: 'es',
+        uiColor: '#9AB8F3'
+    });
+    CKEDITOR.replace('id_short_about', {
+        language: 'es',
+        uiColor: '#9AB8F3'
+    });
 });
