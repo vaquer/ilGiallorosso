3193a4dc57eacf1d76b57716f100d4c06d074962
vaker <akura11.tt@gmail.com>
2016-02-21 23:48:13 +0000

    fixing jinja templates

--- ilGiallorosso/ilGiallorosso/settings/dev.py
+++ ilGiallorosso/ilGiallorosso/settings/dev.py
@@ -57,11 +57,24 @@ MIDDLEWARE_CLASSES = (
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    {
         "BACKEND": "django_jinja.backend.Jinja2",
         "DIRS": [os.path.join(BASE_DIR, '../templates/jinja')],
         "APP_DIRS": True,
         "OPTIONS": {
-        	'context_processors': [
+            'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -111,19 +124,6 @@ TEMPLATES = [
             "auto_reload": DEBUG,
             "translation_engine": "django.utils.translation",
         }
-    },
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
     },
 ]
 
